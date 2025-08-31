501f275f52082dded3ec79ab8c98e93d76a956f8
Tanmoy-Debnath <tanmoy.bubt@gmail.com>
2025-08-31 23:46:47 +0600

    string issue

--- index.php
+++ index.php
@@ -5,5 +5,6 @@ $txt2 = "W3Schools.com";
 
 echo ("$txt1");
 echo "<p>Study PHP at $txt2</p>";
-echo '<p> This is Tanmoy'. '<br>' .$txt1. '</p>'
+echo '<p> This is Tanmoy'. '<br>' .$txt1. '</p>';
+var_dump($txt1);
 ?>
